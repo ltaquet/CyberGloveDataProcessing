969db2f1bc62f511916023c6ef2615dad006475b
ltaquet <ltaquet@mcw.edu>
2021-06-08 11:15:33 -0500

    Creates new patient directory

---
+++ newPatient.m
@@ -0,0 +1,16 @@
+clear; clc;
+curr_dir = pwd;
+PID = input('New patient ID: ','s');
+cd ../../patients
+
+if ~exist(PID, 'dir')
+    mkdir(PID);
+    cd(PID)
+    mkdir('Binary Data');
+    mkdir('cal');
+    mkdir('Calibrated Data');
+    mkdir('Uncalibrated Data');
+else
+    fprintf('This patient already exists\n');
+end
+cd(curr_dir);
